27047e5c2d6756f4dd5f8f1d172e0dc58f452635
Hazem Essam <hazemkwita123@gmail.com>
2021-08-09 00:56:12 +0200

    add choose another stream action to ytb.py

--- README.md
+++ README.md
@@ -63,8 +63,9 @@ Title: Dependency Inversion (DIP) - SOLID Design Principles
 4) webm@160k -> 0.1MB
 Choose Stream: 3
 ----------------
-0) Get Link
-1) Download
+0) Get Direct Link
+1) Download Stream
+2) Choose Another Stream
 Choose Action: 0
 ----------------
 https://manifest.googlevideo.com/api/manifest/dash/expire/1627562151/ei/R0wCYY7XGKqCp-oPzaicgAY/ip/156.197.128.160/id/38a1211200f7b95e/source/youtube/requiressl/yes/playback_host/r1---sn-uxaxjvhxbt2u-2nqd.googlevideo.com/mh/cE/mm/31%2C29/mn/sn-uxaxjvhxbt2u-2nqd%2Csn-hgn7yn7s/ms/au%2Crdu/mv/m/mvi/1/pl/21/tx/24047099/txs/24047098%2C24047099%2C24047100%2C24047101%2C24047102%2C24047103%2C24047104/hfr/all/as/fmp4_audio_clear%2Cwebm_audio_clear%2Cwebm2_audio_clear%2Cfmp4_sd_hd_clear%2Cwebm2_sd_hd_clear/initcwndbps/241250/vprv/1/mt/1627540368/fvip/3/keepalive/yes/fexp/24001373%2C24007246/beids/9466586/itag/0/sparams/expire%2Cei%2Cip%2Cid%2Csource%2Crequiressl%2Ctx%2Ctxs%2Chfr%2Cas%2Cvprv%2Citag/sig/AOq0QJ8wRQIgANFSe7oqw1GDzdXryFx14lW1UQYACFFumnu-FYxjjXUCIQCBp8JZw1PeFsQxEOje1yw3gCerpxiPSqaZjuXxTTzu7Q%3D%3D/lsparams/playback_host%2Cmh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps/lsig/AG3C_xAwRgIhAPp0lbXa14CnHsq7oN8Hn_GWp2rYkNryANgOgvqwMPBiAiEA3yJ94x8Fxkt_RtHfbtUPCon7nnKTQbrey9FPf-_MDd0%3D
--- ytb.py
+++ ytb.py
@@ -9,39 +9,41 @@ from termcolor import colored
 from os import getenv
 
 
+def print_streams():
+    for id, stream in enumerate(streams, 0):
+        stream_size = round(stream.get_filesize() / (1024*1024), 1)
+        if str(stream).startswith('normal:'):
+            stream_resolution = str(stream.quality).split('x')[-1]
+            print(f'{id}) {stream.extension}@{stream_resolution}p -> {stream_size}MB')
+        elif str(stream).startswith('audio:'):
+            print(f'{id}) {stream.extension}@{stream.quality} -> {stream_size}MB')
+
+
 if len(argv) != 2 or not argv[1].startswith('https://www.youtube.com'):
     print('Usage:\n\tytb <youtube video link>')
     exit()
 
 link = argv[1]
 pfy_obj = pafy.new(url=link)
-streams = []
-id = 0
+streams = pfy_obj.streams + pfy_obj.audiostreams
 
 print(f'Title: {pfy_obj.title}')
 print('-' * (7 + len(pfy_obj.title)))
 
-for stream in pfy_obj.streams:
-    streams.append(stream)
-    stream_resolution = str(stream.quality).split('x')[-1]
-    stream_size = round(stream.get_filesize() / (1024*1024), 1)
-    print(f'{id}) {stream.extension}@{stream_resolution}p -> {stream_size}MB')
-    id += 1
+while True:
+    print_streams()
+    
+    stream_id = int(input('Choose Stream: '))
+    print('-' * (15 + len(str(stream_id))))
 
-for stream in pfy_obj.audiostreams:
-    streams.append(stream)
-    stream_size = round(stream.get_filesize() / (1024*1024), 1)
-    print(f'{id}) {stream.extension}@{stream.quality} -> {stream_size}MB')
-    id += 1
+    action = int(input('0) Get Direct Link\n1) Download Stream\n2) Choose Another Stream\nChoose Action: '))
+    print('-' * (15 + len(str(action))))
 
-stream_id = int(input('Choose Stream: '))
-print('-' * (15 + len(str(id))))
+    if action not in [0, 1]: continue
 
-action = int(input('0) Get Link\n1) Download\nChoose Action: '))
-print('-' * (15 + len(str(action))))
-
-if action == 0: print(colored(streams[stream_id].url, 'green'))
-elif action == 1: 
-    download_dir = getenv('DOWNLOADS_DIR')
-    print(f'Downloading to '+ colored(download_dir, 'green'))
-    streams[stream_id].download(filepath=download_dir)
+    if action == 0: print(colored(streams[stream_id].url, 'green'))
+    elif action == 1:
+        download_dir = getenv('DOWNLOADS_DIR')
+        print(f'Downloading to '+ colored(download_dir, 'green'))
+        streams[stream_id].download(filepath=download_dir)
+    break
